1aeea9a0bdcc2611147cfe339ecc0aa8640e55de
Roman_Runkovskyy <romanrynik@gmail.com>
2017-07-14 13:38:55 +0300

    fixed package name

--- src/main/java/ua/windriver/hub/FrontController.java
+++ src/main/java/ua/windriver/hub/FrontController.java
@@ -1,4 +1,4 @@
-package com.epam.windriver.hub;
+package ua.windriver.hub;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/main/java/ua/windriver/hub/HealthStatusModel.java
+++ src/main/java/ua/windriver/hub/HealthStatusModel.java
@@ -1,4 +1,4 @@
-package com.epam.windriver.hub;
+package ua.windriver.hub;
 
 
 import javax.xml.bind.annotation.XmlAccessType;
